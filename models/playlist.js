832019df620b508aa1522202f8c67a221bda5031
ejmeri <elmerisilva@hotmail.com>
2018-03-24 13:39:56 -0300

    error dataa

--- models/playlist.js
+++ models/playlist.js
@@ -1,7 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define("Playlist", {
         id: {
-            type: DataType.INTEGER,
+            type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
