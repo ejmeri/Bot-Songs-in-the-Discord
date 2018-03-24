e67a9968beb10b17ac1764fb0f1c1a8231262023
ejmeri <elmerisilva@hotmail.com>
2018-03-24 14:47:39 -0300

    test sellect pg seque

--- app.js
+++ app.js
@@ -28,21 +28,24 @@ var playlist = require('./models/playlist');
 client.on('warn', console.warn);
 client.on('error', console.error);
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function () {
     client.on('ready', () => {
         console.log('Só vai, to a mil aqui já!');
         client.user.setActivity('Pronto para tocar');
 
-        lineReader.eachLine('autoplaylist.txt', function (line, last) {
-            
-            db.Playlist.create({url:line}).then((err) => {
-                if(err) console.log(err);
-            });
-
-            if (last) {
-                return false; // stop reading
-            }
+        db.Playlist.findAll().then((song) => {
+            console.log(song);
         });
+        //     lineReader.eachLine('autoplaylist.txt', function (line, last) {
+
+        //         db.Playlist.create({url:line}).then((err) => {
+        //             if(err) console.log(err);
+        //         });
+
+        //         if (last) {
+        //             return false; // stop reading
+        //         }
+        //     });
 
     });
 });
