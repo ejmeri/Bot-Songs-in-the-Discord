7a640f3427912eb33b38316473d6a7350e19e684
ejmeri <elmerisilva@hotmail.com>
2018-03-24 13:37:27 -0300

    update model playlist

--- models/playlist.js
+++ models/playlist.js
@@ -1,5 +1,12 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     return sequelize.define("Playlist", {
-      url: DataTypes.STRING
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        url: {
+            type: DataTypes.STRING,
+        } 
     });
 };
